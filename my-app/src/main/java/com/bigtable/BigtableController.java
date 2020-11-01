6591c7623552bf7f40c6ab2189b7e19ae17dc098
Harsh <harsh.26020@gmail.com>
2020-11-01 13:09:55 +0530

    Write in batch

--- my-app/src/main/java/com/bigtable/BigtableController.java
+++ my-app/src/main/java/com/bigtable/BigtableController.java
@@ -2,6 +2,8 @@ package com.bigtable;
 
 import com.google.cloud.bigtable.hbase.BigtableConfiguration;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
@@ -44,11 +46,23 @@ public class BigtableController {
         // [START bigtable_hw_write_rows]
         // Retrieve the table we just created so we can do some reads and writes
         Table table = connection.getTable(TableName.valueOf(TABLE_NAME));
-        for(var row:csv.recordList)
-        {
+        List<Put> putList = new ArrayList<Put>();
+        for (var row : csv.recordList) {
+          System.out.print("Data added to table - ");
+          System.out.print(row);
           Put put = new Put(Bytes.toBytes(row.getUserID()));
-          put.addColumn(COLUMN_FAMILY_NAME, Bytes.toBytes(row.getItemID()), Bytes.toBytes(row.getViewCount()));
-          table.put(put);
+          put.addColumn(
+              COLUMN_FAMILY_NAME,
+              Bytes.toBytes(row.getItemID()),
+              Bytes.toBytes(row.getViewCount()));
+          putList.add(put);
+          if (putList.size() >= 1e6) {
+            table.put(putList);
+            putList.clear();
+          }
+        }
+        if (putList.size() > 0) {
+          table.put(putList);
         }
         String rowKey = "1";
         Result getResult = table.get(new Get(Bytes.toBytes(rowKey)));
