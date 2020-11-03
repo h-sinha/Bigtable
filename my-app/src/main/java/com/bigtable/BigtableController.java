e3526ad8d0446d645451b06cdc92870cc7fb74ce
Anurag Jain <anurag.jain@research.iiit.ac.in>
2020-11-03 13:25:59 +0530

    WIP

--- my-app/src/main/java/com/bigtable/BigtableController.java
+++ my-app/src/main/java/com/bigtable/BigtableController.java
@@ -10,6 +10,7 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
