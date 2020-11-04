a82948ac92b108283aacc6e0ac55f68e2fea6113
Anurag Jain <anurag.jain@research.iiit.ac.in>
2020-11-04 12:26:14 +0530

    WIP

--- my-app/src/main/java/com/bigtable/App.java
+++ my-app/src/main/java/com/bigtable/App.java
@@ -12,7 +12,13 @@ public class App {
     System.out.println("Most popular item in DB = " + inst.popular());
     System.out.print("Top K iterms for userId:1 - ");
     var res = inst.top(1, 2);
-    for (var x : res) System.out.print(x + " ");
+    for (var x : res)
+      System.out.print(x + " ");
+    System.out.println("");
+    System.out.print("Top K interested items for itemId:2 - ");
+    var res2 = inst.top_interested(3, 2);
+    for (var x : res2)
+      System.out.print(x + " ");
     System.out.println("");
     System.exit(0);
   }
