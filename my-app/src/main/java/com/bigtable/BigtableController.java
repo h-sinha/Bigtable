a068c71eb3bf5bc6ae855579b9be5874e02a9470
Harsh <harsh.26020@gmail.com>
2020-11-05 16:46:45 +0530

    Format code

--- my-app/src/main/java/com/bigtable/BigtableController.java
+++ my-app/src/main/java/com/bigtable/BigtableController.java
@@ -139,11 +139,14 @@ public class BigtableController {
           }
         }
       }
-      PriorityQueue<Pair> minHeap = new PriorityQueue<Pair>(k, new Comparator<Pair>() {
-        public int compare(Pair n1, Pair n2) {
-          return n1.v1 - n2.v1;
-        }
-      });
+      PriorityQueue<Pair> minHeap =
+          new PriorityQueue<Pair>(
+              k,
+              new Comparator<Pair>() {
+                public int compare(Pair n1, Pair n2) {
+                  return n1.v1 - n2.v1;
+                }
+              });
       for (Map.Entry mapElement : itemList.entrySet()) {
         int key = (int) mapElement.getKey();
         int value = (int) mapElement.getValue();
