144dd82152d0f7e21242ace4d82c03dd0b91412b
Harsh <harsh.26020@gmail.com>
2020-11-05 13:00:50 +0530

    DEbug heap

--- my-app/src/main/java/com/bigtable/BigtableController.java
+++ my-app/src/main/java/com/bigtable/BigtableController.java
@@ -79,6 +79,7 @@ public class BigtableController {
       Pair pair;
       while ((pair = maxHeap.poll()) != null) {
         ans[idx++] = pair.v2;
+        System.out.println(pair.v1 + " - " + pair.v2);
       }
     } catch (IOException e) {
       System.err.println("Exception while running program: " + e.getMessage());
